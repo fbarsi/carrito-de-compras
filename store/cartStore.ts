f05de6ca4db0f218d47f56866dd6d95613f03d59
fbarsi <francogbarsi@gmail.com>
2026-07-12 12:49:36 -0300

    feat: agrega mmkv para persistencia local al cerrar la aplicacion

--- store/cartStore.ts
+++ store/cartStore.ts
@@ -1,4 +1,6 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+import { zustandStorage } from "./mmkvAdapter";
 
 export type ItemProp = {
   id: string;
@@ -15,22 +17,30 @@ type Cart = {
   clearCart: () => void;
 };
 
-export const useCartStore = create<Cart>((set) => ({
-  items: [],
-  total: 0,
-  addItem: (item) =>
-    set((state) => ({
-      items: [...state.items, item],
-      total: state.total + item.price * item.quantity,
-    })),
-  removeItem: (item) =>
-    set((state) => ({
-      items: state.items.filter((_item) => _item.id !== item.id),
-      total: Math.max(0, state.total - item.price * item.quantity),
-    })),
-  clearCart: () =>
-    set(() => ({
+export const useCartStore = create<Cart>()(
+  persist(
+    (set) => ({
       items: [],
       total: 0,
-    })),
-}));
+      addItem: (item) =>
+        set((state) => ({
+          items: [...state.items, item],
+          total: state.total + item.price * item.quantity,
+        })),
+      removeItem: (item) =>
+        set((state) => ({
+          items: state.items.filter((_item) => _item.id !== item.id),
+          total: Math.max(0, state.total - item.price * item.quantity),
+        })),
+      clearCart: () =>
+        set(() => ({
+          items: [],
+          total: 0,
+        })),
+    }),
+    {
+      name: "cart-storage",
+      storage: createJSONStorage(() => zustandStorage),
+    },
+  ),
+);
